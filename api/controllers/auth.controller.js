a55400a33c91b20181661306b923fa63ed53b67e
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-10-01 00:31:52 +0530

    api: Hash the password using bcryptjs
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,14 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created successfully!");
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 };
