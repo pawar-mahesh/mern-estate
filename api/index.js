4671636da7215c386769491b92c5dc0641fce902
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-10-01 00:23:31 +0530

    api: Create auth router
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import UserRoute from "./routes/user.route.js";
+import userRoute from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,7 +15,10 @@ mongoose
   });
 const app = express();
 
-app.use("/api/user", UserRoute);
+app.use(express.json());
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
