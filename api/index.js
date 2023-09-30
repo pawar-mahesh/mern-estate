f9564b4cad9a597e1099c0b6b7f7a5d0d3327277
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-09-30 22:42:10 +0530

    Install mongoose and dotenv, also configure mongoDB
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.mongodb_connection_string)
+  .then(() => {
+    console.log("Connected to mongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
